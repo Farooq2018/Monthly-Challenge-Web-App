9c7b7b680c113afbe95dd086972c8f5a553d3c63
Farooq2018 <farooqahm97@gmail.com>
2024-03-06 16:30:17 -0600

    Adding templating files

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
+from django.template.loader import render_to_string
 from django.urls import reverse
 
 # Create your views here.
@@ -17,21 +18,26 @@ monthly_challenges = {
     "september": "Learn Django for at least 20 minutes every day!",
     "october": "Eat no meat for the entire month!",
     "november": "Walk for at least 20 minutes every day!",
-    "december": "Learn Django for at least 20 minutes every day!"
+    "december": None
 }
 
 months = list(monthly_challenges.keys())
 
 
 def index(request):
-    list_items = ""
+    #list_items = ""
 
-    for month in months:
-        #month_path = reverse("month_challenge", args=[month])  # challenges/month
-        list_items += f"<li><a href=\"{month}\">{month.capitalize()}</a></li>"
+    #Previously it was loop through the dictionary from below code:
+    # for month in months:
+    #     #month_path = reverse("month_challenge", args=[month])  # challenges/month
+    #     list_items += f"<li><a href=\"{month}\">{month.capitalize()}</a></li>"
+    #
+    # response_data = f"<ul>{list_items}</ul>"
+    # return HttpResponse(response_data)
 
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
 
 
 def monthly_challenge_by_number(request, month):
@@ -49,7 +55,7 @@ def monthly_challenge(request, month):
         # response_data = render_to_string("challenges/challenge.html")
         # return HttpResponse(response_data)
         return render(request, "challenges/challenge.html", {
-            "text": challenge_text, "month": month.capitalize
+            "text": challenge_text, "month": month
         })
     except:
-        return HttpResponseNotFound("<h1>This month is not supported</h1>")
+        raise Http404() #Need to set DEBUG=False for this to work in settings
